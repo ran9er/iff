e13f31c73c622077d6b785dd7fa6b3613fa992eb
ran9er <2999am@gmail.com>
2013-07-01 18:18:10 +0800

    X

---
+++ _lib/sudo.el
@@ -0,0 +1,6 @@
+;;;###autoload
+(defun sudo-edit (&optional arg)
+  (interactive "p")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
